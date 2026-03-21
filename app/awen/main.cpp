015512a4ebbe3cee9b62990446762f6f265413d9
Aaron Shelley <amshelley86@gmail.com>
2026-03-20 20:01:15 -0400

    Fix unused variable with exceptions.

--- app/awen/main.cpp
+++ app/awen/main.cpp
@@ -8,7 +8,8 @@ try
     std::println("Hello, Awen!");
     return EXIT_SUCCESS;
 }
-catch (const std::exception& e)
+catch (const std::exception& /*unused*/)
 {
+    // Can't print since it may throw, but we can log it to a file or something if needed.
     return EXIT_FAILURE;
 }
