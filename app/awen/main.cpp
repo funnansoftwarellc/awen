2d6ac1905b3c03c14f983d643e9532f492008a74
funnan-dev <funnan@funnansoftware.com>
2026-05-15 21:12:17 -0400

    Working raylib.

--- app/awen/main.cpp
+++ app/awen/main.cpp
@@ -1,7 +1,21 @@
-#include <print>
+#include <raylib.h>
+#include <cstdlib>
 
 auto main() -> int
 {
-    std::println("Hello, Awen!");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(1280, 720, "Awen");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Hello, Awen!", 350, 200, 50, ORANGE);
+        DrawFPS(1150, 700);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return EXIT_SUCCESS;
 }
